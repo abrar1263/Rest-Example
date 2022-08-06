8e513113640115e3fd91e650786299ba0a787aa3
Abrar Ansari <52700454+abrar1263@users.noreply.github.com>
2022-08-06 15:41:45 +0530

    Update RestControllerExample.java

--- src/main/java/com/perfectsquare/Restexample/controller/RestControllerExample.java
+++ src/main/java/com/perfectsquare/Restexample/controller/RestControllerExample.java
@@ -1,13 +1,14 @@
 package com.perfectsquare.Restexample.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestControllerExample {
 
-	@GetMapping("/hello.{name}")
-	public String hello(@PathVaribale String name) {
-		return "Welcome "+name;
+	@GetMapping("/hello/{name}")
+	public String hello(@PathVariable String name) {
+		return "Welcome";
 	}
 }
